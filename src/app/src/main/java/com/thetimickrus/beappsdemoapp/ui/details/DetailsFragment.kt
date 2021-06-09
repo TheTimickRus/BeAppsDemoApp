0f5deefe9a26ec4ae38a15b7911bc2d1d49c46be
TheTimickRus <andrey.timofeev.and37@gmail.com>
2021-06-09 21:53:55 +0300

    Init #1

---
+++ src/app/src/main/java/com/thetimickrus/beappsdemoapp/ui/details/DetailsFragment.kt
@@ -0,0 +1,29 @@
+package com.thetimickrus.beappsdemoapp.ui.details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.thetimickrus.beappsdemoapp.R
+
+class DetailsFragment : Fragment() {
+    private lateinit var viewModel: DetailsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DetailsViewModel::class.java)
+
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ src/app/src/main/java/com/thetimickrus/beappsdemoapp/ui/details/DetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.thetimickrus.beappsdemoapp.ui.details
+
+import androidx.lifecycle.ViewModel
+
+class DetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
